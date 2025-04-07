bf0a8d059ca9cd744044ab484842f44c76fd3fc0
ErikOakleaf <oakl33f123@gmail.com>
2025-04-07 15:47:30 +0200

    added get random in zig

--- libraries/io/io.zig
+++ libraries/io/io.zig
@@ -2,6 +2,7 @@ const RESETS_BASE: u32 = 0x4000c000;
 const TIMER_BASE: u32 = 0x40054000;
 const TIMER_TIME_LOW: u32 = (TIMER_BASE + 0x28);
 const TIMER_TIME_HIGH: u32 = (TIMER_BASE + 0x24);
+const RANDOM_BIT: u32 = (0x40060000 + 0x1C);
 
 pub fn write32(address: *volatile u32, value: u32) void {
     address.* = value;
@@ -31,4 +32,8 @@ pub fn readTime() u64 {
     const high64: u64 = @intCast(high);
 
     return (high64 << 32) | low;
+}
+
+pub fn getRandom() usize {
+    return @intCast((read32(@ptrFromInt(RANDOM_BIT))) & 0b1);
 }
---
+++ misctest/random-test-zig/src/main.zig
@@ -0,0 +1,33 @@
+const io = @import("io");
+const uart = @import("uart");
+const GPIO25_CTRL: u32 = 0x0cc;
+const RESETS_BASE: u32 = 0x4000c000;
+const IO_BANK0_BASE: u32 = 0x40014000;
+
+export fn main() linksection(".main") void {
+    uart.uart0Init();
+    io.timerInit();
+
+    io.write32(@ptrFromInt(IO_BANK0_BASE + GPIO25_CTRL), 5);
+    io.write32(@ptrFromInt(0xd0000000 + 0x024), (1 << 25));
+    io.write32(@ptrFromInt(0xd0000000 + 0x14), (1 << 25));
+
+    uart.uartSendU32(io.getRandom());
+    uart.uartSendString("\r\n");
+    uart.uartSendU32(io.getRandom());
+    uart.uartSendString("\r\n");
+    uart.uartSendU32(io.getRandom());
+    uart.uartSendString("\r\n");
+    uart.uartSendU32(io.getRandom());
+    uart.uartSendString("\r\n");
+    uart.uartSendU32(io.getRandom());
+    uart.uartSendString("\r\n");
+    uart.uartSendU32(io.getRandom());
+    uart.uartSendString("\r\n");
+    uart.uartSendU32(io.getRandom());
+    uart.uartSendString("\r\n");
+    uart.uartSendU32(io.getRandom());
+    uart.uartSendString("\r\n");
+    uart.uartSendU32(io.getRandom());
+    uart.uartSendString("\r\n");
+}
